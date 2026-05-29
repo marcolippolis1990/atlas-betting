01842a114500e67d8944c5c93d502d70089e09e5
marcolippolis1990 <marcolippolis1990@gmail.com>
2026-05-29 12:14:59 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,9 +29,13 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30 * 24  # 30 giorni
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
+    # Bcrypt ha un limite di 72 byte
+    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # Bcrypt ha un limite di 72 byte
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
